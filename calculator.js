74bb99ad74ab0cfe6249badfea049b7cefbc9e0b
Zach Miller - Laptop <zach.mx@protonmail.com>
2023-11-23 11:39:54 -0800

    initial calc

---
+++ calculator.js
@@ -0,0 +1,49 @@
+function add(a, b){
+     return (a + b);
+
+}
+
+function subtract(a, b){
+    return (a - b);
+}
+
+function multiply(a, b){
+    return (a * b);
+}
+
+function divide(a, b){
+    return (a / b);
+}
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(operator, a, b){
+    switch (operator){
+        case add:
+            console.log(add(a, b));
+            break
+        case subtract:
+            console.log(subtract(a, b));
+            break
+        case multiply:
+            console.log(multiply(a, b));
+            break
+        case divide:
+            console.log(divide(a, b));
+            break
+    }
+}
+
+let buttonWrapper = document.getElementById('button-wrapper');
+
+buttonWrapper.addEventListener('click', (event) => {
+    const isButton = event.target.nodeName === 'BUTTON';
+    if (!isButton) {
+        return;
+    } else {
+        let btnSelection = event.target;
+        //btnSelection.document.
+    }
+})
